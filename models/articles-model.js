6ea0c9954cd459a0c654effffcd305027469b273
Joseph Kempster <josephkempster@josephs-mbp.home>
2019-04-23 23:03:18 +0100

    Added tests and endpoint for /api/articles/:article_id

--- models/articles-model.js
+++ models/articles-model.js
@@ -20,3 +20,22 @@ exports.fetchAllArticles = ({ username, topic, sort_by, order }) => {
       if (topic) query.where("articles.topic", "=", topic);
     });
 };
+
+exports.fetchArticleById = ({ article_id }) => {
+  console.log(article_id);
+  return connection
+    .select(
+      "articles.author",
+      "title",
+      "articles.article_id",
+      "topic",
+      "articles.created_at",
+      "articles.votes"
+    )
+    .from("articles")
+    .count({ comment_count: "comments.article_id" })
+    .leftJoin("comments", "articles.article_id", "comments.article_id")
+    .groupBy("articles.article_id")
+    .where("articles.article_id", "=", article_id)
+    .then(([result]) => result);
+};
--- routes/articles-router.js
+++ routes/articles-router.js
@@ -1,10 +1,18 @@
 const articlesRouter = require("express").Router();
 const { methodNotAllowed } = require("../errors");
-const { getAllArticles } = require("../controllers/articles-controller");
+const {
+  getAllArticles,
+  getArticleById
+} = require("../controllers/articles-controller");
 
 articlesRouter
   .route("/")
   .get(getAllArticles)
+  .all(methodNotAllowed);
+
+articlesRouter
+  .route("/:article_id")
+  .get(getArticleById)
   .all(methodNotAllowed);
 
 module.exports = articlesRouter;
